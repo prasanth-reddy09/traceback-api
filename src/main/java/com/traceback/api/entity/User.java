0c4aa799a3f0139efc659b00324a754a7acc61f4
prasanth-reddy09 <prasanthreddy4260@gmail.com>
2026-03-10 12:11:55 +0530

    Create User entity and connect to TiDB database

---
+++ src/main/java/com/traceback/api/entity/User.java
@@ -0,0 +1,47 @@
+package com.traceback.api.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(unique = true, nullable = false)
+	private String email;
+	
+	
+	@Column(name = "password_hash")
+    private String passwordHash;
+	
+	@Column(name = "auth_provider")
+    private String authProvider;
+	
+	@CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+	
+}
